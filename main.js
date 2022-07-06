296faf183a4f01528e8d0989dacda9aec52becd8
Kadir0-7 <101948177+Kadir0-7@users.noreply.github.com>
2022-07-05 21:58:36 -0400

    final Commit

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+
+
+
+const url =('https://picsum.photos/v2/list?page=2&limit=100')
+fetch(url) .then( response=> {
+console.log(response)
+return response.json();
+})
+.then(list => {
+  let randomNum = Math.floor(Math.random() *list.length);
+ console.log(list[randomNum])
+ const info= list[randomNum]
+ let picture = "";
+ 
+ list.map(value =>{
+   picture = `<h1>Random picture</h1><img src=${info.download_url} alt="img" height="500px" weight=""500px id="imgs">
+   <p id="img-id">Image ID:${info.id}</p>
+   <p id="author">Author:${info.author}</p>
+   `;
+   console.log(picture);
+  
+ })
+ document.querySelector('#app').innerHTML = picture;
+})
+
+.catch(err => {
+  console.log(err)
+})
